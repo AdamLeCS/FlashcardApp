af70a83ecb9a4669b799a5b9a6fa1675e4ddc99e
Adam Le <adamle1025@gmail.com>
2026-01-19 02:49:38 -0500

    connected startup frame and set frame with buttons

--- src/FlashcardSet.java
+++ src/FlashcardSet.java
@@ -7,16 +7,26 @@ public class FlashcardSet {
     ArrayList<Flashcard> flashcards;
     long lastTimeOpened; // for "recently opened" sorting option
     Path filePath;
+    String name;
 
     FlashcardSet(String setName, Path file) {
         flashcards = new ArrayList<>();
         lastTimeOpened = System.currentTimeMillis(); 
         filePath = file;
+        name = setName;
         // Creates the file for this set in Flashcard_Sets folder
         try {
             Files.createFile(filePath);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Flashcard> getFlashcards() {
+        return this.flashcards;
     }
 }
--- src/StartupFrame.java
+++ src/StartupFrame.java
@@ -1,10 +1,10 @@
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Image;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,14 +15,22 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class StartupFrame extends JFrame implements ActionListener {
+public class StartupFrame extends JFrame{
 
     ArrayList<FlashcardSet> allSets = new ArrayList<>();
+    JPanel cards;
     JButton flashcardSetsButton;
     JButton newSetButton;
+    JButton importSetButton;
+    JButton settingsButton;
+
+    // Card names for card layout
+    final String MAINCARD = "Main card";
+    final String OPENSETCARD = "Open set card";
+    final String INSETCARD = "In set card";
+    final String SETTINGSCARD = "Settings card";
 
     StartupFrame() {
-        
         // This block is for misc window stuff
         // Use .getImage to get the Image from the imageicon, which can be used in the .setIconImage method
         ImageIcon flashcard_app_icon = new ImageIcon("flashcard_app_icon.png"); 
@@ -32,30 +40,41 @@ public class StartupFrame extends JFrame implements ActionListener {
         this.getContentPane().setBackground(Color.LIGHT_GRAY);
         this.setExtendedState(Frame.MAXIMIZED_BOTH);
         this.setMinimumSize(new Dimension(550, 400));
-        this.setLayout(new BorderLayout());
+
+        cards = new JPanel(new CardLayout());
+        // Create the different cards that the card layout will belong to
+        cards.add(mainFrame(), MAINCARD); 
+        cards.add(openSet(), OPENSETCARD);
         
-        // North panel block
+        this.add(cards);
+        this.setVisible(true);
+    }
+    
+    private JPanel mainFrame() {
+         
+        JPanel card = new JPanel(new BorderLayout());
+
+        // North panel block --------------
         JPanel northPanel = new JPanel();
         northPanel.setPreferredSize(new Dimension(100, 70));
-        this.add(northPanel, BorderLayout.NORTH);
+        card.add(northPanel, BorderLayout.NORTH);
         
-        // South panel block
+        // South panel block --------------
         JPanel southPanel = new JPanel();
         southPanel.setPreferredSize(new Dimension(100, 30));
-        this.add(southPanel, BorderLayout.SOUTH);
+        card.add(southPanel, BorderLayout.SOUTH);
         
-        // East panel block
+        // East panel block --------------
         JPanel eastPanel = new JPanel();
         eastPanel.setPreferredSize(new Dimension(70, 100));
-        this.add(eastPanel, BorderLayout.EAST);
+        card.add(eastPanel, BorderLayout.EAST);
 
-        // West panel block
+        // West panel block --------------
         JPanel westPanel = new JPanel();
         westPanel.setPreferredSize(new Dimension(70, 100));
-        this.add(westPanel, BorderLayout.WEST);
+        card.add(westPanel, BorderLayout.WEST);
 
-        // Central panel with main buttons
-
+        // Central panel with main buttons -------------
         JPanel center = new JPanel();
         center.setBackground(Color.WHITE);
         center.setLayout(new GridLayout(2, 2, 10, 10));
@@ -64,30 +83,82 @@ public class StartupFrame extends JFrame implements ActionListener {
         newSetButton = new JButton("Create a new set");
         newSetButton.setFocusable(false);
         newSetButton.setBounds(30, 30, 200, 100);
-        newSetButton.addActionListener(this);
-        center.add(newSetButton);
-        
-        this.add(center, BorderLayout.CENTER);
-        
-
-        this.setVisible(true);
-    }
-
-    // All button methods here    
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == newSetButton) { // Creates a new set if the inputted name is valid
+        newSetButton.addActionListener(e -> {
             String newSetName = JOptionPane.showInputDialog("What do you want the set name to be?");
             Path filePath = Paths.get("./Flashcard_Sets/" + newSetName + ".txt");
             if (!newSetName.equals("") && !Files.exists(filePath)) {
                 FlashcardSet newSet = new FlashcardSet(newSetName, filePath);
-                SetFrame newSetFrame = new SetFrame(newSet);
-                this.dispose();
+                allSets.add(newSet);
+                CardLayout c1 = (CardLayout)(cards.getLayout()); // cards.getLayout returns object of type LayoutManager, so need to type cast
+                c1.show(cards, OPENSETCARD);
             } else if (newSetName.equals("")) {
                 JOptionPane.showMessageDialog(this, "Set names cannot be empty");
             } else {
                 JOptionPane.showMessageDialog(this, "This set name is taken. Try another name!");
             }
-        }
+        });
+        center.add(newSetButton);
+        
+        card.add(center, BorderLayout.CENTER);
+        
+        return card;
+    }
+    
+    private JPanel openSet() {
+        
+        JPanel card = new JPanel(new BorderLayout());
+        
+        // North panel block --------------
+        JPanel northPanel = new JPanel();
+        northPanel.setLayout(null);
+        northPanel.setPreferredSize(new Dimension(100, 70));
+        
+        // Button to go back to main page
+        JButton backButton = new JButton();
+        ImageIcon buttonIcon = resizeImage(40, 40, "images/back_button.png");
+        backButton.setIcon(buttonIcon);
+        backButton.setFocusable(false);
+        backButton.setBounds(85, 15, 40, 40);
+        backButton.addActionListener(e -> {
+            CardLayout c1 = (CardLayout)(cards.getLayout());
+            c1.show(cards, MAINCARD);
+        });
+        northPanel.add(backButton);
+
+        card.add(northPanel, BorderLayout.NORTH);
+        
+        // South panel block -------------
+        JPanel southPanel = new JPanel();
+        southPanel.setPreferredSize(new Dimension(100, 30));
+        card.add(southPanel, BorderLayout.SOUTH);
+        
+        // East panel block --------------
+        JPanel eastPanel = new JPanel();
+        eastPanel.setPreferredSize(new Dimension(70, 100));
+        card.add(eastPanel, BorderLayout.EAST);
+
+        // West panel block -------------
+        JPanel westPanel = new JPanel();
+        westPanel.setPreferredSize(new Dimension(70, 100));
+        card.add(westPanel, BorderLayout.WEST);
+
+        // Central panel with cards
+        JPanel center = new JPanel();
+        center.setBackground(Color.WHITE);
+        center.setLayout(new GridLayout(2, 2, 10, 10));
+        card.add(center, BorderLayout.CENTER);
+
+
+        return card;
+    }
+
+    // Resizes images to fit within their containers correctly
+    private ImageIcon resizeImage(int newWidth, int newHeight, String filePath) {
+        ImageIcon icon = new ImageIcon(filePath);
+        Image originalImage = icon.getImage();
+        Image newImage = originalImage.getScaledInstance(newWidth, newHeight, 0);
+        ImageIcon newIcon = new ImageIcon(newImage);
+
+        return newIcon;
     }
 }
